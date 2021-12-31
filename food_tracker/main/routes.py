9b4405d35dbd562b6472b87256a2aa378fc0fd8b
Charbel <chazar@DESKTOP-C9B46LV.localdomain>
2021-12-31 22:41:53 +1100

    Creating add food function

--- food_tracker/main/routes.py
+++ food_tracker/main/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from sqlalchemy.ext.declarative.api import comparable_using, request
 
 main = Blueprint ("main", __name__)
 
@@ -9,6 +10,14 @@ def index():
 @main.route("/add")
 def add():
     return render_template("add.html")
+
+@main.route("/add", methods=["POST"])
+def add_post():
+    food_name = request.form.get("food-name")
+    proteins = request.form.get("protein")
+    fats = request.form.get("fat")
+    carbs = request.form.get("carbs")
+    return f"<h1>{ food_name } - { proteins } - { carbs } - { fats }</h1>"
 
 @main.route ("/view")
 def view():
